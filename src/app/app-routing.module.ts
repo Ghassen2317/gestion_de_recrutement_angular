da7bd6f8b54cd5cce57fb04bda82fda10448a89b
borgiramzi <ramziborgi3@gmail.com>
2024-06-24 11:08:56 +0100

    home page & espaceEntreprise : done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './home/home-page/home-page.component';
+import { PacksComponent } from './espace-entreprise/packs/packs.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"home",component :HomePageComponent},
+  {path:"espaceEntreprise",component :PacksComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
-import { NgModule } from '@angular/core';
+import { NgModule  } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NavbarComponent } from '../app/components/navbar/navbar.component';
+import{HeroComponent} from '../app/home/hero/hero.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HomePageComponent} from '../app/home/home-page/home-page.component';
+import {CarouselComponent} from '../app/home/carousel/carousel.component';
+import {FooterComponent} from '../app/components/footer/footer.component';
+import {PacksComponent} from '../app/espace-entreprise/packs/packs.component';
+//import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent ,
+    NavbarComponent,
+    HeroComponent,
+    HomePageComponent,
+    CarouselComponent,
+    FooterComponent,
+    PacksComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+   // NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
