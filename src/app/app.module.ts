6aa77f751ffb78833417c18072f8eb9a34dfa5cf
borgiramzi <ramziborgi3@gmail.com>
2024-07-02 11:18:05 +0100

    Resolved merge conflicts and merged khawla into ramzi's work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,9 +15,9 @@ import { SigninComponent } from './authentication/sign-in/sign-in.component';
 import { SignUpComponent } from './authentication/sign-up/sign-up.component';
 import { ResetPasswordComponent } from './authentication/reset-password/reset-password.component';
 import { AddRhComponent } from './espace-entreprise/add-rh/add-rh.component';
-import { FormComponent } from './espace-candidat/form/form.component';
-import { OfferFilterComponent } from './espace-candidat/offer-filter/offer-filter.component';
-import { OfferListComponent } from './espace-candidat/offers-list/offers-list.component';
+import { FormComponent } from './espace-candidat/espace-candidate/form/form.component';
+import { OfferFilterComponent } from './espace-candidat/espace-candidate/offer-filter/offer-filter.component';
+import { OfferListComponent } from './espace-candidat/espace-candidate/offers-list/offers-list.component';
 import { OfferFormComponent } from './espace-entreprise/offer-form/offer-form.component';
 import { OfferCardComponent } from './offer-card/offer-card.component';
 
